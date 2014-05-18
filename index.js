e132b045a4f1c8b65e8783291bd42f80d580e7ba
kaelzhang <i@kael.me>
2014-05-18 12:31:39 +0800

    completes main functionalities and test cases;

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var surl = exports;
+var url = require('url');
+
+
+var REGEX_START_SSH = /^ssh:\/\//;
+surl.parse = function (str) {
+  if (typeof str !== 'string') {
+    throw new TypeError('Parameter \'url\' must be a string');
+  }
+
+  return REGEX_START_SSH.test(str)
+    ? parseSsh(str)
+    : parseScpSsh(str);
+};
+
+surl.format = function (obj) {
+  return obj.protocol
+    ? formatSsh(obj)
+    : formatScpSsh(obj);
+};
+
+function parseSsh (str) {
+  var parsed = url.parse(str);
+  return {
+    protocol: parsed.protocol,
+    user: parsed.auth,
+    hostname: parsed.hostname,
+    pathname: parsed.pathname
+  }
+}
+
+function parseScpSsh(str) {
+  var parsed = url.parse('ssh://' + str);
+  return {
+    protocol: null,
+    user: parsed.auth,
+    hostname: parsed.hostname,
+    pathname: parsed.pathname.replace(/^\/:/, '/')
+  };
+};
+
+function formatSsh (obj) {
+  return obj.protocol + '//' + obj.user + '@' + obj.hostname + obj.pathname;
+}
+
+function formatScpSsh (obj) {
+  return obj.user + '@' + obj.hostname + ':' + obj.pathname.replace(/^\//, '');
+}
--- test/ssh-url.js
+++ test/ssh-url.js
@@ -1,4 +1,39 @@
 'use strict';
 
 var expect = require('chai').expect;
-var ssh_url = require('../');
+var url = require('../');
+
+var cases = [
+{
+  d: 'scp-like syntax',
+  u: 'git@github.com:kaelzhang/node-ssh-url.git',
+  e: {
+    protocol: null,
+    user: 'git',
+    hostname: 'github.com',
+    pathname: '/kaelzhang/node-ssh-url.git'
+  }
+},
+{
+  d: 'ssh protocol',
+  u: 'ssh://git@github.com/kaelzhang/node-ssh-url.git',
+  e: {
+    protocol: 'ssh:',
+    user: 'git',
+    hostname: 'github.com',
+    pathname: '/kaelzhang/node-ssh-url.git'
+  }
+}
+];
+
+
+describe("url.parse(str), url.format(obj)", function(){
+  cases.forEach(function (c) {
+    it(c.d, function(){
+      var parsed = url.parse(c.u);
+      expect(parsed).to.deep.equal(c.e);
+      var formated = url.format(parsed);
+      expect(formated).to.equal(c.u);
+    });
+  });
+});
